8ff9a27c010cb8de2a427fac3e00251f01469512
Jérémie St-Amand <jeremie.stamand@gmail.com>
2022-06-10 20:33:23 -0400

    Missing files

--- src/client/main.c
+++ src/client/main.c
@@ -1,92 +1,195 @@
-#include "graphics.h"
 #include "../common/input.h"
-#include <SDL_events.h>
-#include <SDL_timer.h>
+#include "graphics.h"
+#define WIN32_LEAN_AND_MEAN
+#include <assert.h>
+#include <windows.h>
 
 enum
 {
+	DEFAULT_WINDOW_WIDTH = 1280,
+	DEFAULT_WINDOW_HEIGHT = 720,
+
 	MAX_WINDOW_EVENTS = 8,
 	MAX_KEYBOARD_EVENTS = 64,
 	MAX_MOUSE_EVENTS = 256,
 };
 
-static const u8 scancode_to_key[] = {
-		0, 0, 0, 0, WB_KEY_A, WB_KEY_B, WB_KEY_C, WB_KEY_D, WB_KEY_E, WB_KEY_F, WB_KEY_G, WB_KEY_H, WB_KEY_I, WB_KEY_J, WB_KEY_K, WB_KEY_L, WB_KEY_M,
-		WB_KEY_N, WB_KEY_O, WB_KEY_P, WB_KEY_Q, WB_KEY_R, WB_KEY_S, WB_KEY_T, WB_KEY_U, WB_KEY_V, WB_KEY_W, WB_KEY_X, WB_KEY_Y, WB_KEY_Z, WB_KEY_1, WB_KEY_2,
-		WB_KEY_3, WB_KEY_4, WB_KEY_5, WB_KEY_6, WB_KEY_7, WB_KEY_8, WB_KEY_9, WB_KEY_0, WB_KEY_ENTER, WB_KEY_ESCAPE, WB_KEY_BACKSPACE, WB_KEY_TAB,
-		WB_KEY_SPACE, WB_KEY_MINUS, WB_KEY_EQUAL, WB_KEY_LEFT_BRACKET, WB_KEY_RIGHT_BRACKET, WB_KEY_BACKSLASH, 0, WB_KEY_SEMICOLON,
-		WB_KEY_APOSTROPHE, WB_KEY_GRAVE_ACCENT, WB_KEY_COMMA, WB_KEY_PERIOD, WB_KEY_SLASH, WB_KEY_CAPS_LOCK, WB_KEY_F1, WB_KEY_F2, WB_KEY_F3,
-		WB_KEY_F4, WB_KEY_F5, WB_KEY_F6, WB_KEY_F7, WB_KEY_F8, WB_KEY_F9, WB_KEY_F10, WB_KEY_F11, WB_KEY_F12, WB_KEY_PRINT_SCREEN, WB_KEY_SCROLL_LOCK, WB_KEY_PAUSE,
-		WB_KEY_INSERT, WB_KEY_HOME, WB_KEY_PAGE_UP, WB_KEY_DELETE, WB_KEY_END, WB_KEY_PAGE_DOWN, WB_KEY_RIGHT, WB_KEY_LEFT, WB_KEY_DOWN, WB_KEY_UP,
-		WB_KEY_NUM_LOCK, WB_KEY_KP_DIVIDE, WB_KEY_KP_MULTIPLY, WB_KEY_KP_SUBTRACT, WB_KEY_KP_ADD, WB_KEY_KP_ENTER, WB_KEY_KP_1, WB_KEY_KP_2, WB_KEY_KP_3,
-		WB_KEY_KP_4, WB_KEY_KP_5, WB_KEY_KP_6, WB_KEY_KP_7, WB_KEY_KP_8, WB_KEY_KP_9, WB_KEY_KP_0, WB_KEY_KP_DECIMAL, 0, 0, 0, WB_KEY_KP_EQUAL};
+static const char* k_window_class = "battlecry";
+static const char* k_window_title = "Open Battlecry";
+
+static bool s_quit;
+static bool s_fullscreen;
+static bool s_maximized;
+static RECT s_windowed_rect;
+static int s_window_x;
+static int s_window_y;
+static int s_window_width;
+static int s_window_height;
+
+static void adjustWindow(HWND hwnd)
+{
+	if (s_fullscreen)
+	{
+		// Save the old window rect so we can restore it when exiting fullscreen mode.
+		GetWindowRect(hwnd, &s_windowed_rect);
+
+		// Make the window borderless so that the client area can fill the screen.
+		SetWindowLong(hwnd, GWL_STYLE, WS_SYSMENU | WS_POPUP | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE);
+
+		// Get the settings of the durrent display index. We want the app to go into
+		// fullscreen mode on the display that supports Independent Flip.
+		HMONITOR currentMonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTOPRIMARY);
+		MONITORINFO info;
+		info.cbSize = sizeof(MONITORINFOEX);
+		GetMonitorInfo(currentMonitor, &info);
+
+		s_window_x = info.rcMonitor.left;
+		s_window_y = info.rcMonitor.top;
+
+		int window_width = info.rcMonitor.right - info.rcMonitor.left;
+		int window_height = info.rcMonitor.bottom - info.rcMonitor.top;
+		SetWindowPos(hwnd, HWND_NOTOPMOST, info.rcMonitor.left, info.rcMonitor.top, window_width, window_height, SWP_FRAMECHANGED | SWP_NOACTIVATE);
+
+		ShowWindow(hwnd, SW_MAXIMIZE);
+
+		s_window_width = window_width;
+		s_window_height = window_height;
+	}
+	else
+	{
+		DWORD windowStyle = WS_OVERLAPPEDWINDOW;
+		SetWindowLong(hwnd, GWL_STYLE, windowStyle);
+
+		SetWindowPos(
+				hwnd, HWND_NOTOPMOST, s_windowed_rect.left, s_windowed_rect.top, s_windowed_rect.right - s_windowed_rect.left,
+				s_windowed_rect.bottom - s_windowed_rect.top, SWP_FRAMECHANGED | SWP_NOACTIVATE | SWP_NOOWNERZORDER);
+
+		if (s_maximized)
+		{
+			ShowWindow(hwnd, SW_MAXIMIZE);
+		}
+		else
+		{
+			ShowWindow(hwnd, SW_NORMAL);
+		}
+	}
+}
+
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_NCPAINT:
+		case WM_WINDOWPOSCHANGED:
+		case WM_STYLECHANGED:
+		{
+			return DefWindowProc(hwnd, message, wParam, lParam);
+		}
+		case WM_DISPLAYCHANGE:
+		{
+			adjustWindow(hwnd);
+			break;
+		}
+		case WM_GETMINMAXINFO:
+		{
+			LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
+
+			// Prevent window from collapsing
+			if (!s_fullscreen)
+			{
+				LONG zoom_offset = 128;
+				lpMMI->ptMinTrackSize.x = zoom_offset;
+				lpMMI->ptMinTrackSize.y = zoom_offset;
+			}
+			break;
+		}
+		case WM_ERASEBKGND:
+		{
+			// Make sure to keep consistent background color when resizing.
+			HDC hdc = (HDC)wParam;
+			RECT rc;
+			HBRUSH hbrWhite = CreateSolidBrush(0x00000000);
+			GetClientRect(hwnd, &rc);
+			FillRect(hdc, &rc, hbrWhite);
+			break;
+		}
+		case WM_CLOSE:
+		{
+			PostQuitMessage(0);
+			break;
+		}
+		case WM_DESTROY:
+			break;
+		default:
+			return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
-	wb_graphics_init(&(const wb_graphics_desc)
+	if (argc > 1)
 	{
-		.window_name = "Battlecry"
-	});
-
-	bool quit = false;
-	float delta_time = 0.005f;
-	u64 last_tick = SDL_GetPerformanceCounter();
-	SDL_Event event;
-
-	while (!quit)
-	{
-		wb_input_update(SDL_GetModState());
-
-		while (SDL_PollEvent(&event))
+		for (int i = 1; i < argc; i++)
 		{
-			switch (event.type)
-			{
-			case SDL_KEYUP:
-			case SDL_KEYDOWN:
-				u8 key = scancode_to_key[event.key.keysym.scancode];
-				if (!wb_input_handle_key(key, event.key.type == SDL_KEYUP))
-					quit = true;
-				break;
-			case SDL_MOUSEBUTTONUP:
-			case SDL_MOUSEBUTTONDOWN:
-				u8 mouse_button = event.button.button;
-				wb_input_handle_mouse_button(mouse_button, event.button.type == SDL_MOUSEBUTTONUP);
-				break;
-			case SDL_MOUSEMOTION:
-				wb_input_handle_mouse_motion(event.motion.xrel, event.motion.yrel);
-				break;
-			case SDL_MOUSEWHEEL:
-				wb_input_handle_mouse_wheel(event.wheel.x, event.wheel.y);
-				break;
-			case SDL_WINDOWEVENT:
-				//handleActiveEvent(&event);
-				break;
-			case SDL_TEXTINPUT:
-				//inputhandleText(&event.text);
-				break;
-			case SDL_QUIT:
-				quit = true;
-				break;
-			default:
-				break;
-			}
+			OutputDebugStringA(argv[i]);
 		}
-
-		if (quit)
-			continue;
-
-		wb_graphics_clear();
-
-
-
-		wb_graphics_present();
-
-		u64 tick = SDL_GetPerformanceCounter();
-		u64 delta_tick = tick - last_tick;
-		last_tick = tick;
-		delta_time = (float)(((double)delta_tick * 1000) / (double)SDL_GetPerformanceFrequency());
 	}
 
-	wb_graphics_quit();
+	HINSTANCE instance = GetModuleHandle(NULL);
+	WNDCLASSEX window_class = {
+			.cbSize = sizeof(window_class),
+			.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC,
+			.lpfnWndProc = WndProc,
+			.cbClsExtra = 0,
+			.cbWndExtra = 0,
+			.hInstance = instance,
+			.hIcon = LoadIcon(NULL, IDI_APPLICATION),
+			.hCursor = LoadCursor(NULL, IDC_ARROW),
+			.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH),
+			.lpszMenuName = NULL,
+			.lpszClassName = k_window_class,
+			.hIconSm = 0};
+	RegisterClassEx(&window_class);
+
+	int window_style = WS_OVERLAPPEDWINDOW;
+	RECT window_rect;
+	window_rect.left = 0;
+	window_rect.top = 0;
+	window_rect.right = DEFAULT_WINDOW_WIDTH;
+	window_rect.bottom = DEFAULT_WINDOW_HEIGHT;
+	AdjustWindowRect(&window_rect, window_style, FALSE);
+
+	HWND hwnd = CreateWindowEx(
+			0,
+			k_window_class,
+			k_window_title,
+			window_style,
+			CW_USEDEFAULT, CW_USEDEFAULT,
+			window_rect.right - window_rect.left,
+			window_rect.bottom - window_rect.top,
+			0,
+			NULL,
+			instance,
+			NULL);
+	assert(hwnd != NULL);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	while (!s_quit)
+	{
+		MSG msg = {0};
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+			{
+				s_quit = true;
+			}
+		}
+	}
+
 	return 0;
 }
